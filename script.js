40d42885a19f05137395b18efcacb67c593728cb
aybashir2010 <56992635+aybashir2010@users.noreply.github.com>
2022-01-21 01:35:38 +0100

    all

---
+++ script.js
@@ -0,0 +1,34 @@
+// Set the Summer date
+
+let summerDate = new Date("Jun 1, 2022 15:37:25").getTime();
+
+// Update the count down every 1 second
+let x = setInterval(function() {
+
+  // Get today's date and time
+  let currentDate = new Date().getTime();
+
+  // Find the distance between current date and the summer date
+  let distance = summerDate - currentDate;
+
+  // Time calculations for days, hours, minutes and seconds
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the HTML 
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  // If the count down is finished, write countdown Expired
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("days").innerHTML = "EXPIRED";
+    document.getElementById("hours").innerHTML = "EXPIRED";
+    document.getElementById("minutes").innerHTML = "EXPIRED";
+    document.getElementById("seconds").innerHTML = "EXPIRED";
+  }
+}, 1000);
